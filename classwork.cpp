6e77a1a0c110fa8f6e8fdd241effe0f9b0f3952d
felixp038 <phanfelix1@gmail.com>
2024-12-12 22:44:45 +0000

    fart

--- classwork.cpp
+++ classwork.cpp
@@ -3,17 +3,23 @@
 
 int main()
 {
-  int age;
-  age = 10;
 
-  int gradeLevel = 11;
 
-  int numPets = 0;
-  std::string name = "Felix";
-  std::favorite food = "pizza";
-  std::favorite movie = "N/A";
-  double price = 19.99;
+ int add_xy;
+ int sub_xy;
+ int x = 25;
+ int y = 3;
+ int z = 10;
+ add_xy = x + y;
+ sub_xy = x - y;
 
-  
+ std::cout << add_xy << std::endl << sub_xy << std::endl;
+ 
+ bool x_greater_z = x > z;
+ bool y_greater_x = y > x;
+ std::cout << "x > z is " << (x > z) << std::endl
+           << y_greater_x << std::endl;
+
+
   return 0;
 }
