0f7e52cb176af81179d3db4da1ef077572682c67
felixp038 <phanfelix1@gmail.com>
2024-12-13 22:30:16 +0000

    fart2

--- classwork.cpp
+++ classwork.cpp
@@ -4,22 +4,11 @@
 int main()
 {
 
+bool isSunny = true;
+bool hasUmbrella = false;
 
- int add_xy;
- int sub_xy;
- int x = 25;
- int y = 3;
- int z = 10;
- add_xy = x + y;
- sub_xy = x - y;
-
- std::cout << add_xy << std::endl << sub_xy << std::endl;
- 
- bool x_greater_z = x > z;
- bool y_greater_x = y > x;
- std::cout << "x > z is " << (x > z) << std::endl
-           << y_greater_x << std::endl;
+std::cout << (isSunny || hasUmbrella) << std::endl;
 
 
-  return 0;
+return 0;
 }
